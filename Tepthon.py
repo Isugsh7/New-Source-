7c4323ff0a5573275a62b1df96451881750cc0cf
Isugsh7 <165947173+Isugsh7@users.noreply.github.com>
2024-04-30 07:58:31 +0300

    Update Tepthon.py

--- Tepthon.py
+++ Tepthon.py
@@ -81,7 +81,7 @@ async def _(event):
     ms = (end - start).microseconds / 1000
     await event.edit(f'''
 [Welcome to Source ADNAN]
-☆ VERSION : 4.0
+☆ VERSION : 4.1
 ☆ PING : `{ms}`
 ☆ DATE : `{m9zpi}`
 ☆ ID : `{event.sender_id}`
