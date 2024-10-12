1c0e1e4a354e7dd3d836c40220a6798f810eabc9
Isugsh7 <165947173+Isugsh7@users.noreply.github.com>
2024-10-12 19:14:11 +0300

    Update checktele.py

--- checktele.py
+++ checktele.py
@@ -613,7 +613,7 @@ def gen_user(choice):
         if username in banned[0]:
             c = random.choices(a)
             d = random.choices(eee)
-            d = random.choices(b)
+            s = random.choices(b)
             f = [c[0], s[0], d[0], d[0] , d[0], d[0] ,d[0]]
             random.shuffle(f)
             username = ''.join(f)
