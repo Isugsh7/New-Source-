f1ab90b2cccc329b329902bcd445b359dc26faca
Isugsh7 <165947173+Isugsh7@users.noreply.github.com>
2024-10-16 01:22:50 +0300

    Update checktele.py

--- checktele.py
+++ checktele.py
@@ -718,9 +718,10 @@ def gen_user(choice):
     if choice == "43":
         c = str(''.join((random.choice(a) for i in range(1))))
         d = str(''.join((random.choice(bbb) for i in range(1))))
-        n = str(''.join((random.choice(eee) for i in range(1))))
-        f1 =c+s+s+s+s+s+d
-        f = f1
+        n = str(''.join((random.choice(b) for i in range(1))))
+        f1 = c+s+s+s+s+s+d
+        f2 = c+s+s+s+s+s+n
+        f = f1,f2
         f = random.choice(f)
         username = f
     else:
