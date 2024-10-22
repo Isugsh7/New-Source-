5c7466fa822b07909de4e68b4013efd13a1bf192
Isugsh7 <165947173+Isugsh7@users.noreply.github.com>
2024-10-22 20:50:14 +0300

    Update checktele.py

--- checktele.py
+++ checktele.py
@@ -795,6 +795,35 @@ def gen_user(choice):
             username = username+'bot'
         else:
             pass
+    if choice == "45":
+        c = str(''.join((random.choice(a) for i in range(1))))
+        d = str(''.join((random.choice(b) for i in range(1))))
+        f1 = c+d+d+c+c
+        f2 = c+c+d+d+c
+        f3 = c+c+c+d+d
+        f = f1,f2,f3
+        f = random.choice(f)
+        username = f
+    else:
+        pass
+    if choice == "46":
+        c = str(''.join((random.choice(a) for i in range(1))))
+        d = str(''.join((random.choice(e) for i in range(1))))
+        f1 = c+'_'+d+d+d
+        f = f1
+        f = random.choice(f)
+        username = f
+    else:
+        pass
+    if choice == "47":
+        c = str(''.join((random.choice(a) for i in range(1))))
+        d = str(''.join((random.choice(b) for i in range(1))))
+        f1 = c+c+d+d+d+d+d
+        f = f1,f2,f3
+        f = random.choice(f)
+        username = f
+    else:
+            pass
     return username
 
 @Tepthon.on(events.NewMessage(outgoing=True, pattern=r"\.تشيكر"))
