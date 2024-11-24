6584fe160d2d0d20c145d5d45cb0b44b33be8b57
Isugsh7 <165947173+Isugsh7@users.noreply.github.com>
2024-11-24 20:58:33 +0300

    Update checktele.py

--- checktele.py
+++ checktele.py
@@ -830,12 +830,15 @@ def gen_user(choice):
         d1 = str(''.join((random.choice(b) for i in range(1))))
         d2 = str(''.join((random.choice(b) for i in range(1))))
         d3 = str(''.join((random.choice(b) for i in range(1))))
-        d4 = str(''.join((random.choice(b) for i in range(1))))
         f1 = 'vip'+d1+d2+d1+d1+d1
-        f2= 'id'+d1+d2+d3+d4
-        f3 = 'id'+d1+d2+d3+d3
-        f4 = 't'+d1+d2+d1+d1
-        f = f1,f2,f3,f4
+        f2= 'vip'+d1+d1+d2+d1+d1
+        f3 = 'vip'+d1+d1+d2+d1+d1
+        f4 = 'vip'+d1+d2+d2+d2+d2
+        f5 = 'vip'+d2+d1+d1+d1+d1
+        f6 = 'vip'+c+e+c
+        f7 = 'vip'+c+c+e
+        f8 = 'vip'+c+e+e
+        f = f1,f2,f3,f4,f5,f6,f7,f8
         f = random.choice(f)
         username =f
     else:
