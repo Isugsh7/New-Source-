97629aeb90bec36832cfb491340c3c5f92b6b7fc
Isugsh7 <165947173+Isugsh7@users.noreply.github.com>
2024-11-27 17:13:08 +0300

    Update checktele.py

--- checktele.py
+++ checktele.py
@@ -825,18 +825,29 @@ def gen_user(choice):
         username = f
     else:
         pass
-    if choice == "48":
+    if choice == "50":
         d1 = str(''.join((random.choice(b) for i in range(1))))
         d2 = str(''.join((random.choice(b) for i in range(1))))
         d3 = str(''.join((random.choice(b) for i in range(1))))
         f1 = 'vip'+d1+d2+d1+d1+d1
         f2= 'vip'+d1+d1+d2+d1+d1
-        f3 = 'vip'+d1+d2+d2+d2+d2
-        f4 = 'vip'+d1+d1+d1+d1+d2
-        f = f1,f2,f3,f4
+        f3 = 'vip'+d1+d2+d2+d2+d2 
+        f4 = 'vip'+d1+d2+d2+d2+d2
+        f5 = 'vip'+d1+d1+d1+d1+d2
+        f = f1,f2,f3,f4,f5
         f = random.choice(f)
         username =f
-    else:
+        if username in banned[0]:
+            d1 = str(''.join((random.choice(c) for i in range(1))))
+            d2 = str(''.join((random.choice(e) for i in range(1))))
+            f1 = 'vip'+d1+d2+d1
+            f2= 'vip'+d1+d1+d2
+            f3 = 'vip'+d1+d2+d2
+            f4 = 'vip'+d1+d2+d3
+            f = f1,f2,f3,f4
+            f = random.choice(f)
+            username =f
+        else:
         pass
     if choice == "49":
         c = str(''.join((random.choice(a) for i in range(1))))
