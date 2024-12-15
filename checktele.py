fcc7a06062d33cae9b0964b1fec7de3338c0d296
Isugsh7 <165947173+Isugsh7@users.noreply.github.com>
2024-12-15 19:52:20 +0300

    Update checktele.py

--- checktele.py
+++ checktele.py
@@ -739,8 +739,19 @@ def gen_user(choice):
         f = random.choice(f)
         username = f
     else:
-        pass
+            pass
     if choice == "43":
+        c = str(''.join((random.choice(a) for i in range(1))))
+        d = str(''.join((random.choice(bbb) for i in range(1))))
+        n = str(''.join((random.choice(b) for i in range(1))))
+        f1 = c+s+s+s+s+s+d
+        f2 = c+s+s+s+s+s+n
+        f = f1,f2
+        f = random.choice(f)
+        username = f
+    else:
+        pass
+    if choice == "44":
         c = random.choices(a)
         d = random.choices(b)
         f = [c[0], c[0], d[0], c[0]]
@@ -784,7 +795,7 @@ def gen_user(choice):
             username = username+'bot'
         else:
             pass
-    if choice == "44":
+    if choice == "45":
         c = str(''.join((random.choice(a) for i in range(1))))
         d = str(''.join((random.choice(b) for i in range(1))))
         f1 = c+d+d+c+c
@@ -795,7 +806,7 @@ def gen_user(choice):
         username = f
     else:
         pass
-    if choice == "45":
+    if choice == "46":
         c = str(''.join((random.choice(a) for i in range(1))))
         d = str(''.join((random.choice(e) for i in range(1))))
         f1 = c+'_'+d+d+d
@@ -805,7 +816,7 @@ def gen_user(choice):
         username = f
     else:
         pass
-    if choice == "46":
+    if choice == "47":
         c = str(''.join((random.choice(a) for i in range(1))))
         d = str(''.join((random.choice(b) for i in range(1))))
         f1 = c+c+d+d+d+d+d
