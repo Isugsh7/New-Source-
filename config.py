ac51e8292b17486640615a6d1255f27f9ea72a62
Isugsh7 <165947173+Isugsh7@users.noreply.github.com>
2025-05-14 17:26:19 +0300

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ session = "1BJWap1sAUF8B-YYYtte-w_gCT7Ucb4SsqWC--N4wzwERLhYqByU1LPArKt54Mng2IFc-
 SESSION = "1BJWap1sAUF8B-YYYtte-w_gCT7Ucb4SsqWC--N4wzwERLhYqByU1LPArKt54Mng2IFc-l7Gy1stQonO9RgAns9ZV4-pfjfWkNRA94iQi8dilnRhSz-_18vXFdbkGOx0SkFp6Uvexv6Cm4d2fcu3_UpzRynPLfQZJuCXz1GhbbydEWse8BFXWuL56OGCdNVoR78gG8TiJ1vYgekfRtP-MdjllylT2LDkGu_pYoYnS0qXeAogFzhlkdl-OQtBenR2yYnQ4zX8CVD_5QPh_bD4Q7iReOcyx0xIbbM_HaK-0Pd_C385e-rcLfjj_wlS4ZWLrfAnO5kwEm5fiZsg9ViC-zm9W_1gf36k="  # إذا كان يستخدم في مكان آخر
 token = "7657246738:AAEY-NO6KaCWkxod6N33ENvI-bvucpwLXGs"  # ضع توكن البوت من BotFather هنا (بين علامات اقتباس)
 
-eighthon = TelegramClient(StringSession(session), API_ID, API_HASH)
+Tepthon = TelegramClient(StringSession(session), API_ID, API_HASH)
 bot = TelegramClient("bot", API_ID, API_HASH).start(bot_token=token)
 ispay = ['yes']
 ispay2 = ['yes']
